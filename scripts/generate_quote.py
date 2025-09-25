d5543a86e915e7c29fc9d08cd417334d66c469bb
Md.Rafiuzzaman Khan <ponickid@gmail.com>
2025-09-25 12:42:11 +0600

    Update quote API endpoint and response handling

--- scripts/generate_quote.py
+++ scripts/generate_quote.py
@@ -17,10 +17,10 @@ def write_to_readme(static_readme , quote):
     open('./README.md' , 'w').writelines(lines)
 
 def main():
-    url_eng = "https://api.adviceslip.com/advice"
+    url_eng = "https://programming-quotesapi.vercel.app/api/random"
     quote_response = getResponse(url_eng)
-    context = quote_response['advice']
-    author = Advice #quote_response['author']
+    context = quote_response['quote']
+    author = quote_response['author']
     quote = context + " - " + author
     # print(quote)
     static_readme = './static_readme.md'
